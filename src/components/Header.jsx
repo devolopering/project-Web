5d37c8439f39b506de2781d083c7c60ef77dc358
ID22663 <gayratyoldoshboyev3@gmail.com>
2024-09-29 09:05:25 +0500

    adit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import logo from '/logo.svg';
+import { TbPhoneCall } from "react-icons/tb";
 
 function Header() {
   const links = [
@@ -13,24 +14,30 @@ function Header() {
   ];
 
   return (
-    <header className="bg-[#00000005]  py-7 ">
-      <div className=" max-w-[1261px] px-5 mx-auto flex justify-between items-center">
+    <header className="bg-[#00000005] py-7">
+      <div className="max-w-[1261px] px-5 mx-auto flex justify-between items-center">
         <NavLink to={'/'}>
           <img src={logo} alt="my_site_logo" />
         </NavLink>
 
-     
         <nav>
           <ul className="flex space-x-6">
             {links.map((link, index) => (
-              <li key={index}>
+              <li key={index} className="flex items-center">
                 <NavLink 
                   to={link === 'Home' ? '/' : `/${link.replace(/\s+/g, '-').toLowerCase()}`} 
                   className={({ isActive }) => 
                     isActive ? "text-[#F7F8F9]" : "text-[#D9D9D9] hover:text-[#F7F8F9]"
                   }
                 >
-                  {link}
+                  {link === 'Contact us' ? (
+                    <span className="bg-[#FD560B] px-[38px] py-[16px] text-white rounded-[7px] text-nowrap flex items-center shadow-[0_4px_10px_rgba(255,116,41,0.3)]">
+                      {link}
+                      <TbPhoneCall className="ml-2 text-white" />
+                    </span>
+                  ) : (
+                    link
+                  )}
                 </NavLink>
               </li>
             ))}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,61 +1,103 @@
 import React from 'react';
-import HomeHero from '/truck.png'; 
+import HomeHero from '/truck.svg'; 
 import Success from '/truck_n2.png'; 
-import { Button, Flowbite } from 'flowbite-react';
-import { FaArrowUpRightFromSquare } from "react-icons/fa6";
-
+import { Button, Flowbite } from 'flowbite-react'; 
+import { FaArrowUpRightFromSquare } from "react-icons/fa6"; 
 
 function Home() {
+  
+  const customTheme = {
+    button: {
+      color: {
+        primary: "w-[273px] py-2 bg-[#FD560B] text-nowrap rounded-[7px]", 
+        secondary: "py-2 w-[197px] bg-[#F1F1F1] text-black text-nowrap rounded-[7px]", 
+      },
+    },
+  };
 
-    const customTheme = {
-        button: {
-          color: {
-            primary: " w-[273px] py-2 bg-[#FD560B] text-nowrap ",
-            secondary: " py-2 w-[197px] bg-[#F1F1F1] text-black text-nowrap ",
-          },
-        },
-      };
   return (
-    <div className="relative  max-w-[1261px]  mx-auto px-5 ">
-      <div 
-        style={{ backgroundImage: `url(${HomeHero})` }} 
-        className=' bg-center bg-cover flex items-center'
-      >
-       <div >
-       <div className="text-white">
-          <p className="text-lg mb-4">We Keep Your Truck Running, Anytime, Anywhere</p>
-          <h1 className='text-7xl font-bold'>
-            Efficient & Reliable <br /> 
-            <span className='text-[#FD560B]'>Logistics Services</span><br /> 
-            Tailored for Your <br />
-            <span >
-              Success
-              <img src={Success} alt="Success bg" className='absolute  translate-y-[-70px] translate-x-[120px]' />
-            </span>
-          </h1>
-          <div className='flex items-center gap-2 mt-6'>
-            <div className='bg-[#6A2709] flex items-center justify-center rounded-full h-6 w-6'>
-              <span className='bg-[#FD560B] rounded-full w-[14px] h-[14px]'></span>
-            </div>
-            <p className='text-[#C7C7CC]'>
-              We offer comprehensive solutions for all your logistics needs, from transportation <br /> to dispatch and accounting. Experience the J&J Bros advantage today!
+    <div className=" w-[100%] bg-cover  bg-left "  style={{ backgroundImage: `url(${HomeHero})` }}> 
+      <div className="max-w-[1261px] mx-auto px-5 flex items-center "> 
+        <div className="flex items-start">
+          
+          
+          <div className="text-white">
+            <p className="text-lg mb-4">
+              We Keep Your Truck Running, Anytime, Anywhere 
             </p>
-         
-          </div>
-          <Flowbite theme={{theme: customTheme}} >
-            <div className='flex items-center mt-[60px]'>
-            <Button color={"primary"}> <span className='flex items-center'>Get Started<FaArrowUpRightFromSquare/></span> </Button>
-            <Button color={"secondary"}>Learn More</Button>
+            <h1 className="text-7xl font-bold">
+              Efficient & Reliable <br />
+              <span className="text-[#FD560B]">Logistics Services</span><br />
+              Tailored for Your <br />
+              <span>
+                Success
+                <img src={Success} alt="Success bg" className="absolute translate-y-[-70px] translate-x-[120px]" />
+              
+              </span>
+            </h1>
+
+          
+            <div className="flex items-center gap-2 mt-6">
+              <div className="bg-[#6A2709] flex items-center justify-center rounded-full h-6 w-6">
+                <span className="bg-[#FD560B] rounded-full w-[14px] h-[14px]"></span>
+              </div>
+              <p className="text-[#C7C7CC]">
+                We offer comprehensive solutions for all your logistics needs, from transportation <br /> 
+                to dispatch and accounting. Experience the J&J Bros advantage today! 
+              </p>
             </div>
+
+      
+            <Flowbite theme={{ theme: customTheme }}>
+              <div className="flex items-center mt-[60px] gap-[26px]">
+                <Button color="primary">
+                  <span className="flex items-center rounded-lg shadow-[0_4px_10px_rgba(255,186,111,0.3)]">
+                    Get Started <FaArrowUpRightFromSquare className="ml-2" /> 
+                  </span>
+                </Button>
+                <Button color="secondary" className="rounded-lg shadow-[0_4px_10px_rgba(255,255,255,0.3)]">
+                  Learn More
+                </Button>
+              </div>
             </Flowbite>
+          </div>
+
+         
+          <div
+            className="relative "
+          >
+            
+            <div className="bg-[rgba(255,255,255,0.08)] absolute top-0 left-0 max-w-min flex items-center gap-1 px-3 rounded">
+              <span className="w-[7px] h-[7px] rounded-full bg-[#FD560B]"></span>
+              <p className="text-white text-lg">Dispatching</p> 
+            </div>
+            
+            <div className="bg-[rgba(255,255,255,0.08)] absolute top-4 left-60 max-w-min flex items-center gap-1 px-3 rounded">
+              <span className="w-[7px] h-[7px] rounded-full bg-[#FD560B]"></span>
+              <p className="text-white text-[17px] text-nowrap">ELD Management</p> 
+            </div>
+            
+            <div className="bg-[rgba(255,255,255,0.08)] absolute top-[117px] left-8 max-w-min flex items-center gap-1 px-3 rounded">
+              <span className="w-[12px] h-[12px] rounded-full bg-[#FD560B]"></span>
+              <p className="text-white text-[32px] text-nowrap">ELD Service</p> 
+            </div>
+            
+            <div className="bg-[rgba(255,255,255,0.08)] absolute top-[244px] left-[343px] max-w-min flex items-center gap-1 px-3 rounded">
+              <span className="w-[12px] h-[12px] rounded-full bg-[#FD560B]"></span>
+              <p className="text-white text-[32px]">Fleet</p> 
+            </div>
+
+            <div className="bg-[rgba(255,255,255,0.08)] absolute top-[285px] left-[185px] max-w-min flex items-center gap-1 px-3 rounded">
+              <span className="w-[9px] h-[9px] rounded-full bg-[#FD560B]"></span>
+              <p className="text-white text-[24px]">Safety</p> 
+            </div>
+            
+            <div className="bg-[rgba(255,255,255,0.08)] absolute top-[352px] left-5 max-w-min flex items-center gap-1 px-3 rounded">
+              <span className="w-[7px] h-[7px] rounded-full bg-[#FD560B]"></span>
+              <p className="text-white text-[18px]">Updating</p> 
+            </div>
+          </div>
         </div>
-        <div>
-        <div>
-            <span className='w-[7px] h-[7px] rounded-full bg-[#FD560B] '></span>
-            <p>Dispatching</p>
-        </div>
-        </div>
-       </div>
       </div>
     </div>
   );
