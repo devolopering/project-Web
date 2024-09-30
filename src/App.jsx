4186aafb8bc4f4946b7e449feb996962b19e608b
Shohjahon <shohjahonabdusamadov07gmail.com>
2024-09-30 20:29:59 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Header from './components/Header'
-import Home from './pages/Home'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
-   <div className='bg-[#070606] max-w-[1440px] mx-auto '>
-    <BrowserRouter>
-    <Header/>
-    <Routes>
-    <Route path='/' element={<Home/>} />
-    </Routes>
-
-    </BrowserRouter>
-    
-   </div>
-  )
+    <div className="bg-[#070606] max-w-[1440px] mx-auto ">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
